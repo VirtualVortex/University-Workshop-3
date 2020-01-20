8d5ee59c9028abbdb7cb8db31e0cae7562cff780
VirtualVortex <mcoates111@gmail.com>
2020-01-20 19:13:57 +0000

    Adding project to the repository
    Signed-off-by: VirtualVortex <mcoates111@gmail.com>

---
+++ COMP280_1_1800868_Workshop_3/Assets/COMP280Materials/Scripts/FireProjectile.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireProjectile : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject projectile;
+    [SerializeField]
+    private float delay;
+
+    [Header("Direction")]
+
+    [SerializeField]
+    float x;
+    [SerializeField]
+    float y;
+    [SerializeField]
+    float z;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine("FireObject");
+    }
+
+    //Continuasly run code in coroutine
+    public IEnumerator FireObject() 
+    {
+        while (true) 
+        {
+            yield return new WaitForSeconds(delay);
+            Launch();
+        }
+    }
+
+    //Spawn fireball, apply force to object and then destroy it after 10 seconds
+    void Launch() 
+    {
+        GameObject magma = Instantiate(projectile, transform.position, Quaternion.identity);
+        magma.GetComponent<Rigidbody>().AddForce(new Vector3(Random.Range(-x, x), y, Random.Range(-z, z)), ForceMode.Impulse);
+        Destroy(magma, 10);
+    }
+}
